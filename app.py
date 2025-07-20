898008a20ce8716a8eccb936daedbc286cac9197
SofiaIsabelli <sofiacarvalho21092008@gmail.com>
2025-07-20 20:39:43 -0300

    Adiciona o session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = 'sua_chave_secreta_aqui'
 
@@ -8,6 +8,8 @@ def index():
 
 @app.route('/registrarUsuario', methods=['GET', 'POST'])
 def registrarUsuario():
+    if 'usuarios' not in session:
+        session['usuarios'] = []
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
@@ -17,13 +19,21 @@ def registrarUsuario():
         if not username or not email or not password:
             flash('Todos os campos são obrigatórios!', 'danger')
         else:
+            novo_usuario = {
+                'username': username,
+                'email': email,
+                'password': password
+            }
+            usuarios = session['usuarios']
+            usuarios.append(novo_usuario)
+            session['usuarios'] = usuarios
             flash(f'Usuário {username} cadastrado com sucesso!', 'success')
             return redirect(url_for('registrarUsuario'))
     
 # flash(): Armazena uma mensagem que será exibida na próxima requisição. Isso
 # é útil para fornecer mensagens ao usuário.
 
-    return render_template("formulario.html")
+    return render_template("formulario.html", usuarios=session.get('usuarios', []))
                            
 
 if __name__ == '__main__':
